bd3f264729cad6f0ddf6964cde2e09e0c5d12ec6
tahsinctg <nishatsikder@hotmai.com>
2015-09-09 00:39:40 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,51 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+        $(".embed-responsive iframe").addClass("embed-responsive-item");
+        $(".carousel-inner .item:first-child").addClass("active");
+        
+        $('[data-toggle="tooltip"]').tooltip();
+
+
+        $(".header-area").sticky({topSpacing: 0});
+
+        // jQuery smooth scroll
+        $('li.smooth-scroll a').bind('click', function(event) {
+            var $anchor = $(this);
+            var headerH = '60';
+            $('html, body').stop().animate({
+                scrollTop : $($anchor.attr('href')).offset().top - headerH + "px"
+            }, 1200, 'easeInOutExpo');
+
+            event.preventDefault();
+        });  
+
+        // jQuery scroll spy
+	        $('body').scrollspy({ 
+	            target: '.navbar-collapse',
+	            offset: 95
+        });   
+        
+	        $('.parralax-bg').scrolly({bgParallax: true});
+
+	        $(".navbar-toggle").click(function(){
+	        	$("body").addClass("mobile-menu-activated");
+	        });
+
+	        $("ul.nav.navbar-nav li a").click(function(){
+	        	$(".navbar-collapse").removeClass("in");
+	        });
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
